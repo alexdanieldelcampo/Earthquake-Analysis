398fdd89b3849fd8971eb43e16ac5ce450f6b258
Alexander del Campo <Alexander@macbook-pro.lan>
2021-04-18 13:40:40 -0700

    fixed interactivity/added Outdoors tile

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -75,7 +75,8 @@ function createFeatures(earthquakeData, plates) {
         return {
             color:"orange",
             fillColor: "white",
-            fillOpacity:0
+            fillOpacity:0,
+            interactive: false
         }
     },      
     onEachFeature: function(feature,layer){
@@ -147,9 +148,23 @@ function createMap(earthquakes, plates) {
     accessToken: API_KEY
   });
 
+
+  mapbox://styles/mapbox/outdoors-v11
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+
+
+
+
+
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
+    "Outdoors Map": outdoormap,
     "Dark Map": darkmap
   };
 
